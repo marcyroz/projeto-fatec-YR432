2e6cbf148f806b7c87b6d128e39c991464d1db1f
Johny Richard Rodrigues da Silva <johnyrichard213@gmail.com>
2025-03-17 10:49:56 -0300

    Sistema de salvar as caracteristica das entidades
    Sistema de salvar as caracteristica das entidades

--- assets/scripts/components/redBloodCell/scriptRBC.ts
+++ assets/scripts/components/redBloodCell/scriptRBC.ts
@@ -5,6 +5,8 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
 
+import SaveSystem from "../../SaveSystem";
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -22,14 +24,37 @@ export default class scriptRBC extends cc.Component {
     @property
     infected: boolean = false;
 
+    public saveData(): void {
+        SaveSystem.saveEntity(this, 'RBCData');
+    }
+
+    public loadData(): void {
+        const data = SaveSystem.loadEntity('RBCData');
+        if (data) {
+            this.health = data.health;
+            this.speed = data.speed;
+            this.resistence = data.resistence;
+            this.infected = data.infected;
+        }
+    }
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
 
     start () {
-
+        this.loadData();
     }
 
     // update (dt) {}
+    // Aqui a gente vai usar para configurar o intervalo para salvar e também a mudança de atributos
+    update(dt: number) {
+        
+        this.health -= dt * 0.1;
+        this.speed += dt * 0.1;
+
+        // Salva os dados a cada frame (ou em um intervalo específico)
+        this.saveData();
+    }
 }
+
