82f88e93c0b6e28c55fa587f39b81d3a647f9481
Marcy <marcelly.nascimento84@gmail.com>
2025-03-19 10:00:51 -0300

    fix: ajustando tamanho de sprites e correção de bugs na janela pop up

--- assets/scripts/components/window/PopUpWindow.ts
+++ assets/scripts/components/window/PopUpWindow.ts
@@ -10,30 +10,17 @@ export default class NewClass extends cc.Component {
 
   private isOpen: boolean = false;
   private isAnimating: boolean = false;
-  private maskNode: cc.Node = null;
 
   onLoad() {
     this.node.active = false;
-    this.createMask();
-  }
-
-  createMask() {
-    this.maskNode = new cc.Node('Mask');
-    let maskSprite = this.maskNode.addComponent(cc.Sprite);
-    maskSprite.spriteFrame = null;
-    this.maskNode.setContentSize(cc.winSize);
-    this.node.parent.addChild(this.maskNode);
-    this.maskNode.zIndex = this.node.zIndex - 1;
-    this.maskNode.active = false;
   }
 
   Show_Window() {
-    if (this.isAnimating || this.isOpen) return; // Agora não fecha ao clicar de novo
+    if (this.isAnimating || this.isOpen) return;
 
     this.isOpen = true;
     this.isAnimating = true;
     this.node.active = true;
-    this.maskNode.active = true;
 
     this.node.opacity = 0;
     this.node.scale = 0.2;
@@ -55,7 +42,6 @@ export default class NewClass extends cc.Component {
       .to(0.5, { opacity: 0, scale: 0.2 }, { easing: 'quartInOut' })
       .call(() => {
         this.node.active = false;
-        this.maskNode.active = false;
         this.isAnimating = false;
       })
       .start();
--- assets/scripts/components/window/dropDownMenu/dropDown.ts
+++ assets/scripts/components/window/dropDownMenu/dropDown.ts
@@ -9,12 +9,10 @@ export default class DropdownMenu extends cc.Component {
   private isAnimating: boolean = false;
 
   onLoad() {
-    this.dropdownContent.active = false; // Começa fechado
+    this.dropdownContent.active = false;
 
-    // Adiciona evento de clique no botão do dropdown
     this.node.on(cc.Node.EventType.TOUCH_END, this.toggleDropdown, this);
 
-    // Adiciona evento de clique fora para fechar o dropdown
     cc.director
       .getScene()
       .on(cc.Node.EventType.TOUCH_START, this.onGlobalClick, this);
@@ -60,7 +58,6 @@ export default class DropdownMenu extends cc.Component {
   }
 
   onGlobalClick(event: cc.Event.EventTouch) {
-    // Verifica se o clique foi dentro do dropdown
     let clickInside = this.node
       .getBoundingBoxToWorld()
       .contains(event.getLocation());
